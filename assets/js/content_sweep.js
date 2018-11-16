4c4663a9b40a468a4a8738427b80b16b69850438
Eric C Schoenborn <cultureondemand@gmail.com>
2018-11-16 14:43:54 -0500

    js

---
+++ assets/js/content_sweep.js
@@ -0,0 +1,175 @@
+var slideData = [
+
+  {
+    section: "",
+    header: "",
+    bkgImage: {
+      small: "Denver_capitol.jpg",
+      medium: "Denver_capitol.jpg",
+      large: "Denver_capitol.jpg"
+    },
+    body: `
+
+
+  <span class="opening">
+  <p>Anyone who travels on highways in the Front Range knows </p>
+  </span>
+
+
+
+<p>
+The traditional solution, expanding the highway network to relieve congestion and accommodate 
+ 
+<br />
+
+    `
+  },
+  
+
+
+  {
+    section: "",
+    header: "",
+    bkgImage: {
+      small: "sweep02.png",
+      medium: "sweep02.png",
+      large: "sweep02.png"
+    },
+    body: `
+ <span class="opening">
+  <p>What if there was a way to increase mobility on the existing road, and improve transit and other options, wi</p>
+</span>
+<p>
+The idea actually is v
+</p>
+     `
+  },
+  
+
+
+
+  {
+    section: "",
+     header: "",
+    bkgImage: {
+      small: "sweep03.png",
+      medium: "sweep03.png",
+      large: "sweep03.png"
+    },
+    body: `
+ <span class="opening">
+  <p>But it is expensive to add new lanes, and in many places physical cons</p>
+</span>
+
+ 
+`
+  },
+  
+
+
+
+
+  {
+    section: "Figure 1 | Annual Hours of Delay, Denver-Aurora <span class='footnotenumber'>2</span><br /> <div id='outputB'><div id='fnA'><span class='footnotenumberr'>2</span> Texas A&M Transportation Institute. 2015.  <a href='https://mobility.tamu.edu/ums/'>Annual Urban Mobility Scorecard.</a></div></div>",
+   header: "",
+    bkgImage: {
+       small: "",
+      medium: "",
+      large: ""
+    },
+    body:"<a href='https://www.youtube.com/watch?v=1PnWbXnnr4g'>This U.S. Department of Transportation video illustrates</a> the intensity and complexity of adding new lanes to Interstate 25 through southeast metro Denver, known as the T-REX project. Lack of funds and the high cost of acquiring more land for right-of-ways make adding even more lanes cost-prohibitive."
+
+   },
+  {
+    section: "Figure 2 | Colorado Population, 2000-2050 <span class='footnotenumber'>3</span><br /> <div id='outputB'><div id='fnA'><span class='footnotenumberr'>3</span> Colorado State Demography Office. 2017.  <a href='https://demography.dola.colorado.gov/population/population-totals-colorado-substate/#population-totals-for-'>Population Forecasts.</a></div></div>",
+   header: "",
+    bkgImage: {
+      small: "",
+      medium: "",
+      large: ""
+    },
+    body: `
+     As population and employment increases along the corridor, issues of congestion are only expected to become worse.
+    `
+  },
+  {
+    section: "Figure 3 | Colorado State Motor Fuel Taxes in Real Dollars, 1993 and 2015 <span class='footnotenumber'>4</span><br /> <div id='outputB'><div id='fnA'><span class='footnotenumberr'>4</span> Federal Highway Administration. Highway Statistics. 1993 and 2015. <a href='https://www.fhwa.dot.gov/policyinformation/statistics.cfm'>Table SF-1 Motor Fuel Taxes.</a> and US Inflation Calculator. 2017. <a href='http://www.usinflationcalculator.com/'>Inflation Calculator.</a></div></div>",
+   header: "",
+    bkgImage: {
+      small: "",
+      medium: "",
+      large: ""
+    },
+    body: `
+ Between 1993 (when Colorado last raised its gas tax) and 2015, the nominal value of the money CDOT collected rose by about $200 million dollars, but in real value the revenue has actually decreased by $21 million.    `
+  },
+
+    {
+    section: "Figure 4 | Managed Lanes and Interstate Expansion Costs in the Denver Metro Area",
+    header: "",
+    bkgImage: {
+      small: "map_mobi.png",
+      medium: "map_mobi.png",
+      large: "map.png"
+    },
+    body: `CDOT faces significant funding challenges and lacks the funding to complete other major highway expansions that are already planned (I-70 East, I-25 North, I-70 Mountain, and I-270).`
+    
+  },
+  {
+    section: "Figure 5 | Congestion on the T-REX Corridor <span class='footnotenumber'>5</span><br /> <div id='outputB'><div id='fnA'><span class='footnotenumberr'>5</span> Volume/Capacity Ratio data was obtained for the entire corridor from CDOT.</div></div>",
+  header: "",
+    bkgImage: {
+      small: "",
+      medium: "",
+      large: ""
+    },
+    body:`By 2010, just four years after completion of the $1.67 billion  Transportation Expansion (T-REX) project, congestion had returned to the level experienced when construction began in 2001.`
+
+  } ,
+  {
+    section: "",
+header: "",
+    bkgImage: {
+      small: "",
+      medium: "",
+      large: ""
+    },
+
+    body:`
+            
+ 
+<p>&nbsp;</p>
+
+<h3 class='prog'>
+
+
+Saving money and protecting the environment by advancing energy efficiency in Arizona, Colorado, Nevada, New Mexico, Utah and Wyoming</h3>
+Southwest Energy Efficiency Project
+<br />
+<a href='http://www.swenergy.org/' target='_blank'>www.swenergy.org</a>
+
+<br />
+<a href="//www.facebook.com/SWenergyefficiency/" class="social-icon" aria-label="Facebook" alt="Facebook">
+<img src="assets/images/facebook-box.png" height="21" border="0" alt="facebook"></a>
+          
+<a href="//www.twitter.com/southwestee" class="social-icon" aria-label="Twitter" alt="Twitter">
+<img src="assets/images/twitter.png" height="21" border="0" alt="facebook"></a>
+
+<br />
+<br />
+ 
+<p>&nbsp;</p>
+<h4><a href="assets/sweep_improving_mobility_wout_more_lanes.pdf">Improving Mobility Without Building More Lanes</a></h4>
+<button class="mdc-button mdc-button--raised" data-demo-no-js style="font-size:135%;font-weight:800; " onclick="window.open('assets/sweep_improving_mobility_wout_more_lanes.pdf')">
+Download the Report
+</button>
+
+
+ `
+
+  }
+
+
+
+
+];
